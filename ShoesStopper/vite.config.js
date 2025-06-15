112ebca4952d8d5541f044bd4d090beffeb67968
Gourav-Dubey <161192077+Gourav-Dubey@users.noreply.github.com>
2025-06-15 16:07:12 +0530

    corrected viteconfig

--- ShoesStopper/vite.config.js
+++ ShoesStopper/vite.config.js
@@ -5,14 +5,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  server: {
-    // this helps during development
-    historyApiFallback: true,
-  },
-  build: {
-    // helps during deployment on Netlify/Vercel
-    rollupOptions: {
-      input: "/index.html",
-    },
-  },
+
 });
